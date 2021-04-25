2febebf6c364df00bcb559e32462cd18ebd5800f
Георгий Исаев <georgiyelbaf@gmail.com>
2021-04-26 00:16:05 +0300

    Add Лабораторная 04 (full)

---
+++ ITMO.CSCourse2021.Labs.Lab04._01/Test.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Labs.Lab04._01
+{
+    /// <summary>
+    /// This the test harness
+    /// </summary>
+
+    public class Test
+    {
+        public static void Main()
+        {
+            /*Упражнение 1. Использование параметров в методах, возвращающих значения*/
+            int x; // Input value 1
+            int y; // Input value 2
+            int greater; // Result from Greater()
+
+            // Get input numbers
+            Console.WriteLine("Enter first number:");
+            x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            y = int.Parse(Console.ReadLine());
+
+            // Test the Greater( ) method
+            greater = Utils.Greater(x, y);
+            Console.WriteLine("The greater value is " + greater);
+            Console.ReadKey();
+
+            /*Упражнение 2. Использование в методах параметров, передаваемых по ссылке*/
+            // Test the Swap method
+            Console.WriteLine("Before swap: " + x + "," + y);
+            Utils.Swap(ref x, ref y);
+            Console.WriteLine("After swap: " + x + "," + y);
+            Console.ReadKey();
+
+            /*Упражнение 3. Использование возвращаемых параметров в методах*/
+            int f; // Factorial result
+            bool ok; // Factorial success or failure
+
+            Console.WriteLine("Number for factorial:");
+            x = int.Parse(Console.ReadLine());
+
+            // Test the factorial function
+            ok = Utils.Factorial(x, out f);
+            // Output factorial results
+            if (ok)
+                Console.WriteLine("Factorial(" + x + ") = " + f);
+            else
+                Console.WriteLine("Cannot compute this factorial");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ITMO.CSCourse2021.Labs.Lab04._01/Utils.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Labs.Lab04._01
+{
+    class Utils
+    {
+        //
+        // Return the greater of two integer values
+        //
+
+        public static int Greater(int a, int b)
+        {
+            if (a > b)
+                return a;
+            else
+                return b;
+        }
+        public static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        public static bool Factorial(int n, out int answer)
+        {
+            int k; // Loop counter
+            int f; // Working value
+            bool ok = true; // True if okay, false if not
+
+            if (n < 0)
+                ok = false;
+            try
+            {
+                checked
+                {
+                    f = 1;
+                    for (k = 2; k <= n; ++k)
+                    {
+                        f = f * k;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                f = 0;
+                ok = false;
+            }    
+            answer = f;    
+            return ok;
+        }
+    }
+}
+
