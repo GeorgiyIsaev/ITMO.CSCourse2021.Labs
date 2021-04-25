78c66efd3191d1ffee788059263a119b3d5e208e
Георгий Исаев <georgiyelbaf@gmail.com>
2021-04-25 23:26:58 +0300

    Лабораторная 03 упражнение 01

---
+++ ITMO.CSCourse2021.Labs.Lab03_ForEnum/WhatDay.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Labs.Lab03_ForEnum
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    class WhatDay
+    {
+        static void Main()
+        {
+
+            /*Упражнение 1. Преобразование дня года в дату типа  месяц - день
+              Второй вид решения  */
+            Console.Write("Please enter a day number between 1 and 365: ");
+            string line = Console.ReadLine();
+            int dayNum = int.Parse(line);
+            int monthNum = 0;
+
+
+            /*В лабораторной работе пропущена коллекция месяцов!! Напомнить преподу!*/
+            var DaysInMonths = new int[] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            foreach (int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+            Console.WriteLine("{ 0} { 1}", dayNum, monthName);
+        }
+…
+}
+
+}
