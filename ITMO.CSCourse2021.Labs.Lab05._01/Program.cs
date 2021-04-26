d28d6702f01fc9259013c5fc882ba0ca0008e314
Георгий Исаев <georgiyelbaf@gmail.com>
2021-04-26 03:02:27 +0300

    Мелкие правки

--- ITMO.CSCourse2021.Labs.Lab05._01/Program.cs
+++ ITMO.CSCourse2021.Labs.Lab05._01/Program.cs
@@ -18,17 +18,18 @@ namespace ITMO.CSCourse2021.Labs.Lab05._01
                 Console.WriteLine(arg);
             }
             Console.ReadKey();
-            /* Запуска приложения с аругументами
-             * 1. Открыть командную строку
-             * 2. Кинуть скомпилированый файл .exe в командную строку
-             * 3. После полного имяни через пробел ввести несколько аругметов:
+            /* Запуска приложения с аргументами
+            * Вариант 01: 1. Открыть командную строку
+            * 2. Кинуть скомпилированный файл .exe в командную строку
+            * 3. После полного имени файла через пробел ввести несколько аргументов:
             C:\Users\Isaev>C:\Repository\ITMO.CSCourse2021.Labs\ITMO.CSCourse2021.Lab
             s.Lab05._01\bin\Debug\ITMO.CSCourse2021.Labs.Lab05._01.exe help notHelp
-             * Варинт 02: Создать ярылк .exe-файла, в свойствах у полного имени объекта
-             прописать аргументы после знака тире:  
-            C:\Repository\ITMO.CSCourse2021.Labs\ITMO.CSCourse2021.Labs.Lab0
-            5._01\bin\Debug\ITMO.CSCourse2021.Labs.Lab05._01.exe -help nothelp
-             */
+            * Вариант 02: Создать ярылк .exe-файла, в свойствах у полного имени 
+            * объекта прописать аргументы после знака тире:  
+            C:\Repository\ITMO.CSCourse2021.Labs\ITMO.CSCourse2021.Labs.Lab05._01
+            \bin\Debug \ITMO.CSCourse2021.Labs.Lab05._01.exe -help nothelp
+            */
+
 
         }
     }
