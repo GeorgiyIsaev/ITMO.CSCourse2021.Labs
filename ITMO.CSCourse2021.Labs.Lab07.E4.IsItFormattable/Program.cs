ac9a60e8ed4dbd8486803448501eae3aba92f258
Георгий Исаев <georgiyelbaf@gmail.com>
2021-04-29 15:26:15 +0300

    Add Лабораторная 7 -Упражнение 4

---
+++ ITMO.CSCourse2021.Labs.Lab07.E4.IsItFormattable/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Лабораторная работа 7. Создание и использование ссылочных переменных*/
+/*Упражнение 4. Проверка наличия реализации интерфейса*/
+
+namespace ITMO.CSCourse2021.Labs.Lab07.E4.IsItFormattable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            ulong ul = 0;
+            string s = "Test";
+
+            Console.WriteLine("int: {0}", Utils.IsItFormattable(i));
+            Console.WriteLine("ulong: {0}", Utils.IsItFormattable(ul));
+            Console.WriteLine("String: {0}", Utils.IsItFormattable(s));
+
+
+        }
+    }
+}
