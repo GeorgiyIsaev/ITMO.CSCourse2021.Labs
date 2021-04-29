e200c8f4ec61b68b922bc5949e224f73a7508154
Георгий Исаев <georgiyelbaf@gmail.com>
2021-04-29 15:04:43 +0300

    Add Лабораторная 7 - Упражнение 2

---
+++ ITMO.CSCourse2021.Labs.Lab07.E2.StringConversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Labs.Lab07.E2.StringConversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message;
+
+            // Get an input string
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
+
+            // Reverse the string
+            Utils.Reverse(ref message);
+
+            // Display the result
+            Console.WriteLine(message);
+            Console.ReadKey();
+
+        }
+    }
+}
