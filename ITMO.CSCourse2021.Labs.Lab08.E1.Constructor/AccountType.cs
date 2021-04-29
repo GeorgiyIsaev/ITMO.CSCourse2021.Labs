7551bb276ef419f03bb54cf42c122abfc7e6fa8c
Георгий Исаев <georgiyelbaf@gmail.com>
2021-04-29 20:55:36 +0300

    Add Лабораторная8 - Упражнение 1

---
+++ ITMO.CSCourse2021.Labs.Lab08.E1.Constructor/AccountType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITMO.CSCourse2021.Labs.Lab08.E1.Constructor
+{
+
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+}
