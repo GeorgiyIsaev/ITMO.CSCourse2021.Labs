2a8b7c0ebeb4607730e86f168dabeaac5df6f71b
Георгий Исаев <georgiyelbaf@gmail.com>
2021-04-29 15:39:56 +0300

    Add Лабораторная 7 - Упражнение 5 (Full)

---
+++ ITMO.CSCourse2021.Labs.Lab07.E5.WorkInterface/Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*Лабораторная работа 7. Создание и использование ссылочных переменных*/
+/*Упражнение 5. Работа с интерфейсами*/
+
+namespace ITMO.CSCourse2021.Labs.Lab07.E5.WorkInterface
+{
+    class Test
+    {
+        public static void Main()
+        {
+            int num = 65;
+            string msg = "A String";
+            Coordinate c = new Coordinate(21.0, 68.0);
+
+            Utils.Display(num);
+            Utils.Display(msg);
+            Utils.Display(c);
+        }
+    }
+}
