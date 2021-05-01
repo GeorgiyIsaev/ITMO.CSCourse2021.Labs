bfdb95a5cf83f54640367373b584c99b2fac1b27
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-02 00:58:42 +0300

    Add Lab13.E2 (Full)

--- ITMO.CSCourse2021.Labs.Lab13.E2.DeveloperInfoAttribute/TestRational.cs
+++ ITMO.CSCourse2021.Labs.Lab13.E2.DeveloperInfoAttribute/TestRational.cs
@@ -4,12 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*Лабораторная работа 13. Создание и использование атрибутов*/
+/*Упражнение 2. Создание и использование пользовательского атрибута*/
+
 namespace ITMO.CSCourse2021.Labs.Lab13.E2.DeveloperInfoAttribute
 {
     class TestRational
     {
         static void Main(string[] args)
         {
+       
+            System.Reflection.MemberInfo attrInfo;
+            attrInfo = typeof(Rational);
+            object[] attrs = attrInfo.GetCustomAttributes(false);
+
+            foreach (CustomAttribute.DeveloperInfoAttribute devAttr in attrs)
+            {
+                Console.WriteLine("Developer: {0}\tDate: {1}", devAttr.Developer, devAttr.Date);
+            }           
+
         }
     }
 }
