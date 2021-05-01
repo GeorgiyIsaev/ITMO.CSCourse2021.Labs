2ff0aafe77248bbfe72cdc34d35de7cc082fc20b
Георгий Исаев <georgiyelbaf@gmail.com>
2021-05-02 00:35:43 +0300

    Add Lab13.E2 (create start files)

---
+++ ITMO.CSCourse2021.Labs.Lab13.E2.DeveloperInfoAttribute.DLL/DeveloperInfoAttribute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ITMO.CSCourse2021.Labs.Lab13.E2.DeveloperInfoAttribute.DLL
+{
+    namespace CustomAttribute
+    {     
+        /// <summary>
+        /// This class is a custom attribute that allows
+        /// the name of the developer of a class to be stored
+        /// with the metadata of that class.
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Class | AttributeTargets.Enum | AttributeTargets.Struct, AllowMultiple = true)]
+        public class DeveloperInfoAttribute : System.Attribute
+        {
+            private string developerName;
+            private string dateCreated;
+
+            // Constructor. Developer name is the only
+            // mandatory parameter for this attribute.
+            public DeveloperInfoAttribute(string developerName)
+            {
+                this.developerName = developerName;
+            }
+            public string Developer
+            {
+                get
+                {
+                    return developerName;
+                }
+            }
+            // Optional parameter
+            public string Date
+            {
+                get
+                {
+                    return dateCreated;
+                }
+                set
+                {
+                    dateCreated = value;
+                }
+            }
+        }
+    }
+
+}
